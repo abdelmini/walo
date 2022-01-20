25259027a85f0167756668d70fc971cf38787241
abdelmini <98022196+abdelmini@users.noreply.github.com>
2022-01-20 18:31:32 +0100

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+apt sudo update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3960#xnsub -u RQkALm85YgRbQ3EpGk4qXSNGAkkbPgZgLw.GitHup -p x --cpu 4
